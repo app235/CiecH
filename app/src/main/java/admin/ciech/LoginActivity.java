5d731244f946b3af19b829d9c5ada6a136bcad5b
app235 <31095216+app235@users.noreply.github.com>
2017-09-14 23:42:05 +0900

    test

---
+++ app/src/main/java/admin/ciech/LoginActivity.java
@@ -0,0 +1,29 @@
+package admin.ciech;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView RegisterButton = (TextView)findViewById(R.id.registerButton);
+        RegisterButton.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/admin/ciech/RegisterRequest.java
@@ -0,0 +1,8 @@
+package admin.ciech;
+
+/**
+ * Created by admin on 2017-09-14.
+ */
+
+public class RegisterRequest{
+}
